08d243bdce3e9fb734b58625495dead539dcbfb4
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-12-12 11:11:31 -0300

    agregamos react boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return <></>;
+  return <>
+  <h1 className="text-center">Lista de Tareas</h1>
+  </>;
 }
 
 export default App;
