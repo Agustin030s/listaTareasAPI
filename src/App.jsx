10bf53dc4613295d3157183e0218915d92fc05b9
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-12-19 11:21:26 -0300

    agregamos el componente formulario tarea

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Container } from "react-bootstrap";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./Components/Footer";
+import FormularioTarea from "./Components/FormularioTarea";
 
 function App() {
   return (
     <>
       <Container className="my-4 mainPage">
         <h1 className="text-center text-light display-3">Lista de Tareas</h1>
+        <FormularioTarea></FormularioTarea>
       </Container>
       <Footer></Footer>
     </>
--- src/Components/FormularioTarea.jsx
+++ src/Components/FormularioTarea.jsx
@@ -1,10 +1,17 @@
+import { Button, Form } from 'react-bootstrap';
+// import Form from 'react-bootstrap/Form';
 
 const FormularioTarea = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <section>
+      <Form>
+        <Form.Group className="mb-3 d-flex" controlId="inputTarea">
+          <Form.Control type="text" placeholder="Ej: Tarea 1" className='me-2'/>
+          <Button type='submit'>Agregar</Button>
+        </Form.Group>
+      </Form>
+    </section>
+  );
 };
 
 export default FormularioTarea;
