5d22446313b057e6a11d421989e1e239d5445fa3
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2024-03-12 19:24:19 -0300

    se agrego el boton para editar una tarea

--- src/Components/ItemTarea.jsx
+++ src/Components/ItemTarea.jsx
@@ -1,9 +1,25 @@
 import { Button, ListGroup } from "react-bootstrap";
 
-const ItemTarea = ({tarea, borrarTarea}) => {
+const ItemTarea = ({ tarea, borrarTarea }) => {
   return (
     <ListGroup.Item className="d-flex justify-content-between align-items-center">
-      {tarea} <Button variant="danger" onClick={() => borrarTarea(tarea)}>Borrar</Button>
+      {tarea}
+      <div className="d-flex justify-content-center align-items-center flex-wrap">
+        <Button
+          variant="warning"
+          className="mx-md-1 mb-2 mb-md-0"
+          onClick={() => borrarTarea(tarea)}
+        >
+          <i class="bi bi-pencil-square"></i>
+        </Button>
+        <Button
+          variant="danger"
+          className="mx-md-1"
+          onClick={() => borrarTarea(tarea)}
+        >
+          <i class="bi bi-trash3-fill"></i>
+        </Button>
+      </div>
     </ListGroup.Item>
   );
 };
